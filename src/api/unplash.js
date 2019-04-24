fc61022a15e2f20973c40563a83a49d186ee1035
Artyom Kreynes <kreynes@Artyoms-MacBook-Pro.local>
2019-04-24 15:36:56 +0300

    Added unplash api

---
+++ src/api/unplash.js
@@ -0,0 +1,10 @@
+const getPhotos = async term => {
+  return await (await fetch(
+    `https://api.unsplash.com/search/photos?client_id=${
+      process.env.REACT_APP_UNSPLASH_API_KEY
+    }&query=${term}`,
+    { method: "get" }
+  )).json();
+};
+
+export default { getPhotos };
